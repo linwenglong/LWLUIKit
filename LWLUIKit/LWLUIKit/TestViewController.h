bb0d351fc6c8db665fe23b7dd201fc07e9c7fd6c
linwenlong <576682208@qq.com>
2019-04-16 09:08:03 +0800

    第二次测试，谢谢

---
+++ LWLUIKit/LWLUIKit/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  LWLUIKit
+//
+//  Created by linwenlong on 2019/4/16.
+//  Copyright © 2019 林文龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LWLUIKit/LWLUIKit/TestViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestViewController.m
+//  LWLUIKit
+//
+//  Created by linwenlong on 2019/4/16.
+//  Copyright © 2019 林文龙. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view.backgroundColor = [UIColor redColor];
+    view.center = self.view.center;
+    [self.view addSubview:view];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
